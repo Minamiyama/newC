77aa8be357c7f200c026f49770552846cc679a01
Minamiyama <minamiyama@qq.com>
2018-09-29 17:31:17 +0800

    合约完成

---
+++ Truffle/contracts/TreePresell.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+contract TreePresell {
+    uint constant TREE_TYPE = 8;
+    uint constant TREE_AMOUNT = 6;
+    uint constant TREE_PRICE = 64 ether;
+
+    address owner;
+
+    address[][TREE_TYPE] private treeOwners;
+
+    constructor() public payable {
+        owner = msg.sender;
+    }
+
+    function buy(uint treeType) public payable returns (uint[TREE_TYPE], uint[TREE_TYPE]) {
+        require(msg.value == TREE_PRICE);
+        require(treeType >= 0 && treeType < TREE_TYPE);
+        require(treeOwners[treeType].length < TREE_AMOUNT);
+        
+        treeOwners[treeType].push(msg.sender);
+        owner.transfer(msg.value);
+
+        return getSellInfo();
+    }
+
+    function getSellInfo() public view returns (uint[TREE_TYPE], uint[TREE_TYPE]) {
+        uint[TREE_TYPE] memory trees;
+        uint[TREE_TYPE] memory mine;
+        for (uint i = 0; i < TREE_TYPE; i++) {
+            trees[i] = TREE_AMOUNT - treeOwners[i].length;
+            mine[i] = 0;
+            for (uint index = 0; index < treeOwners[i].length; index++) {
+                if (treeOwners[i][index] == msg.sender) {
+                    mine[i]++;
+                }
+            }
+        }
+        return (trees, mine);
+    }
+
+    function getBalance() public view returns (uint) {
+        require(msg.sender == owner);
+        return address(this).balance;
+    }
+}
---
+++ Truffle/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var TreePresell = artifacts.require("TreePresell");
+
+module.exports = function(deployer) {
+    deployer.deploy(TreePresell);
+};
